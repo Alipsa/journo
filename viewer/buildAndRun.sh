fce86c199c5599e9e893ff890e5546a57466b35d
per <per@alipsa.se>
2024-03-17 10:43:22 +0100

    isolate mac specific options

--- viewer/buildAndRun.sh
+++ viewer/buildAndRun.sh
@@ -4,6 +4,20 @@ if command -v jdk21; then
   source jdk21
 fi
 
-JAVA_OPTS="-Xmx8g -Xdock:name=journo -Xdock:icon=./src/main/assembly/mac/journo.icns"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  OS=linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  OS=mac
+else
+  # msys, cygwin, win32
+  OS=win
+fi
+
+JAVA_OPTS="-Xmx8g"
+
+if [[ "${OS}" == "mac" ]]; then
+  JAVA_OPTS="$JAVA_OPTS -Xdock:name=journo -Xdock:icon=./src/main/assembly/mac/journo.icns"
+fi
+
 mvn package || exit 1
 java $JAVA_OPTS -jar target/journo-viewer-0.6.0-SNAPSHOT.jar
