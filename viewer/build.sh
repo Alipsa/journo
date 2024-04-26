11b6938942165299a123743f6ce8b9bf993a09bd
pernyf <per.nyfelt@nordnet.se>
2024-04-26 17:44:44 +0200

    allow jdk 17 if present

--- viewer/build.sh
+++ viewer/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-if command -v jdk21; then
-  source jdk21
-fi
 
 if command -v java ; then
 	javaVersion=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
@@ -9,6 +6,11 @@ if command -v java ; then
 	  echo "Java $javaVersion OK"
 	else
 	  echo "Java version 17 or greater required"
+	  if command -v jdk21; then
+      source jdk21
+    elif command -v jdk17; then
+      source jdk17
+    fi
 	  exit 1
 	fi
 else
