05caa6d8d15c26a44913da1f1cf4158eff39ffad
pernyf <per.nyfelt@nordnet.se>
2025-01-02 11:49:54 +0100

    fix release script

--- release.sh
+++ release.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-
+set -e
+BASEDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if command -v jdk21; then
   . jdk21
 fi
 mvn -Prelease clean site deploy
-echo "Now build the fatjars for upload to github"
+echo "creating the viewer zip"
+cd "$BASEDIR/viewer"
+source ./createApp.sh skipInstructions
+echo "build the runtime fatjar"
+cd "$BASEDIR/runtime"
 mvn -DskipTests -Pfatjar -q package
 echo "Released and ready for manual release at github!"
