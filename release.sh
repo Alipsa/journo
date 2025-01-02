5e988a8e0fef6a5c239e14a97f0083d413c73e38
pernyf <per.nyfelt@nordnet.se>
2025-01-02 12:24:44 +0100

    enhance release descriptions

--- release.sh
+++ release.sh
@@ -12,3 +12,8 @@ echo "build the runtime fatjar"
 cd "$BASEDIR/runtime"
 mvn -DskipTests -Pfatjar -q package
 echo "Released and ready for manual release at github!"
+echo "Upload the following"
+echo "- runtime/target/journo-runtime-[version]-javadoc.jar"
+echo "- runtime/target/journo-runtime-[version]-jar-with-dependencies.jar"
+echo "- viewer/target/journo-viewer.zip"
+echo "- viewer/journoViewer.xml"
