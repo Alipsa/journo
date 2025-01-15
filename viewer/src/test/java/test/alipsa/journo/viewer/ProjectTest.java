91ec57f434779f7c3c0c304e5b5157ec706c6026
pernyf <per.nyfelt@nordnet.se>
2025-01-15 11:28:16 +0100

    change project test to use target dir instead of tmp

--- viewer/src/test/java/test/alipsa/journo/viewer/ProjectTest.java
+++ viewer/src/test/java/test/alipsa/journo/viewer/ProjectTest.java
@@ -19,19 +19,20 @@ public class ProjectTest {
 
   @Test
   public void testStoreAndLoad() throws IOException {
-    Path path = Paths.get("/tmp/JournoTest.prj");
+    Path basePath = Paths.get(".").toAbsolutePath();
+    Path path = basePath.resolve(Paths.get( "target/JournoTest.prj"));
     Project p = new Project();
     p.setName("Test project");
-    p.setTemplateFile(Paths.get("/tmp/JournoTest.ftl"));
-    p.setDataFile(Paths.get("/tmp/JournoTest.groovy"));
+    p.setTemplateFile(basePath.resolve(Paths.get("target/JournoTest.ftl")));
+    p.setDataFile(basePath.resolve(Paths.get("target/JournoTest.groovy")));
 
     Project.save(p, path);
-    System.out.println(Files.readString(path));
+    //System.out.println(Files.readString(path));
 
     Project p2 = Project.load(path);
     assertEquals("Test project", p2.getName());
-    assertEquals("/tmp/JournoTest.ftl", p2.getTemplateFile().toString());
-    assertEquals("/tmp/JournoTest.groovy", p2.getDataFile().toString());
+    assertEquals(basePath.resolve(Paths.get("target/JournoTest.ftl")).normalize(), p2.getTemplateFile());
+    assertEquals(basePath.resolve(Paths.get("target/JournoTest.groovy")).normalize(), p2.getDataFile());
     assertIterableEquals(Collections.emptyList(), p2.getDependencies());
 
     Properties props = new Properties();
@@ -41,28 +42,28 @@ public class ProjectTest {
     assertEquals("Test project", props.getProperty("name"));
     assertEquals("JournoTest.ftl", props.getProperty("templateFile"));
     assertEquals("JournoTest.groovy", props.getProperty("dataFile"));
-
-    new File("/tmp/JournoTest.prj").deleteOnExit();
   }
 
   @Test
   public void testStoreAndLoadSpread() throws IOException {
-    Path path = Paths.get("/tmp/JournoTest2.prj");
+    Path basePath = Paths.get(".").toAbsolutePath();
+    Path path = basePath.resolve(Paths.get("target/JournoTest2.prj"));
     Project p = new Project();
     p.setName("Test2 project");
-    p.setTemplateFile(Paths.get("/tmp/foo/JournoTest.ftl"));
-    p.setDataFile(Paths.get("/tmp/bar/JournoTest.groovy"));
-    p.setDependencies(List.of(Paths.get("/tmp/dep/jkl.jar"), Paths.get("/tmp/dep/lkn.jar")));
+    p.setTemplateFile(basePath.resolve(Paths.get("target/foo/JournoTest.ftl")));
+    p.setDataFile(basePath.resolve(Paths.get("target/bar/JournoTest.groovy")));
+    p.setDependencies(List.of(basePath.resolve(Paths.get("target/dep/jkl.jar")), basePath.resolve(Paths.get("target/dep/lkn.jar"))));
 
     Project.save(p, path);
-    System.out.println(Files.readString(path));
+    //System.out.println(Files.readString(path));
 
     Project p2 = Project.load(path);
     assertEquals("Test2 project", p2.getName());
-    assertEquals("/tmp/foo/JournoTest.ftl", p2.getTemplateFile().toString());
-    assertEquals("/tmp/bar/JournoTest.groovy", p2.getDataFile().toString());
+    assertEquals(basePath.resolve(Paths.get("target/foo/JournoTest.ftl")).normalize(), p2.getTemplateFile());
+    assertEquals(basePath.resolve(Paths.get("target/bar/JournoTest.groovy")).normalize(), p2.getDataFile());
     assertEquals(2, p2.getDependencies().size());
-    assertIterableEquals(List.of(Paths.get("/tmp/dep/jkl.jar"), Paths.get("/tmp/dep/lkn.jar")),
+    //System.out.println(p2.getDependencies());
+    assertIterableEquals(List.of(basePath.resolve(Paths.get("target/dep/jkl.jar")).normalize(), basePath.resolve(Paths.get("target/dep/lkn.jar")).normalize()),
         p2.getDependencies());
 
     Properties props = new Properties();
@@ -74,6 +75,6 @@ public class ProjectTest {
     assertEquals("bar/JournoTest.groovy", props.getProperty("dataFile"));
     assertEquals("dep/jkl.jar;dep/lkn.jar", props.getProperty("dependencies"));
 
-    new File("/tmp/JournoTest.prj").deleteOnExit();
+    //new File("/tmp/JournoTest.prj").deleteOnExit();
   }
 }
